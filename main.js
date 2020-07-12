5f417c5cb97f397a9e2b8c18b00b21cb6acbc0ef
mm <programinmin@gmail.com>
2020-07-12 22:54:00 +0900

    game complete

--- main.js
+++ main.js
@@ -16,50 +16,83 @@ var counts2 = 0;
 // 魚種
 var ap_fish1 = [
   {
-    "species" : "エゾメバル", "size_out1": "10", "size_out2": "6", "size_in": "60", "pic": "gazou/mazo.png", "time": "5000","size_mean": "15","size_sd": "3"
+    "species" : "エゾメバル1", "size_out1": "10", "size_out2": "2", "size_in": "60", "pic": "gazou/mazo.png", "time": "5000","size_mean": "15","size_sd": "2"
+  },
+  {
+    "species" : "エゾメバル2", "size_out1": "11", "size_out2": "2", "size_in": "75", "pic": "gazou/mazo2.png", "time": "4000","size_mean": "15","size_sd": "2"
+  },
+  {
+    "species" : "エゾメバル3", "size_out1": "13", "size_out2": "3", "size_in": "85", "pic": "gazou/mazo3.png", "time": "3000","size_mean": "15","size_sd": "2"
   }
 ]
 var ap_fish2 = [
   {
-    "species" : "エゾメバル", "size_out1": "10", "size_out2": "6", "size_in": "60", "pic": "gazou/mazo.png", "time": "5000","size_mean": "15","size_sd": "3"
+    "species" : "エゾメバル", "size_out1": "10", "size_out2": "4", "size_in": "60", "pic": "gazou/mazo.png", "time": "5000","size_mean": "15","size_sd": "3"
   },
   {
     "species" : "オニカジカ", "size_out1": "10", "size_out2": "4", "size_in": "75", "pic": "gazou/oni.png", "time": "4000","size_mean": "20","size_sd": "2.5"
   },
   {
-    "species" : "イソバテング", "size_out1": "10", "size_out2": "4", "size_in": "80", "pic": "gazou/iso.png", "time": "4000","size_mean": "20","size_sd": "2.5"
+    "species" : "イソバテング", "size_out1": "10", "size_out2": "3", "size_in": "80", "pic": "gazou/iso.png", "time": "4000","size_mean": "20","size_sd": "2.5"
   },
   {
-    "species" : "ゴッコ", "size_out1": "10", "size_out2": "4", "size_in": "75", "pic": "gazou/gok.png", "time": "4000","size_mean": "27.5","size_sd": "6.125"
+    "species" : "ゴッコ", "size_out1": "10", "size_out2": "3", "size_in": "75", "pic": "gazou/gok.png", "time": "4000","size_mean": "27.5","size_sd": "6.125"
   },
   {
-    "species" : "アイナメ", "size_out1": "15", "size_out2": "6", "size_in": "75", "pic": "gazou/aina.png", "time": "3000","size_mean": "35","size_sd": "10"
+    "species" : "アイナメ", "size_out1": "13", "size_out2": "3", "size_in": "75", "pic": "gazou/aina.png", "time": "3000","size_mean": "35","size_sd": "10"
   },
   {
-    "species" : "シワイカナゴ", "size_out1": "6", "size_out2": "4", "size_in": "75", "pic": "gazou/ikana.png", "time": "3000","size_mean": "8.5", "size_sd": "0.75"
+    "species" : "シワイカナゴ", "size_out1": "6", "size_out2": "3", "size_in": "75", "pic": "gazou/ikana.png", "time": "3000","size_mean": "8.5", "size_sd": "0.75"
   },
   {
-    "species" : "ヒラメ", "size_out1": "20", "size_out2": "6", "size_in": "90", "pic": "gazou/hira.png", "time": "3000","size_mean": "45","size_sd": "10"
+    "species" : "ヒラメ", "size_out1": "15", "size_out2": "4", "size_in": "90", "pic": "gazou/hira.png", "time": "3000","size_mean": "45","size_sd": "10"
   },
   {
-    "species" : "ダンゴウオ", "size_out1": "3", "size_out2": "3", "size_in": "80", "pic": "gazou/favi.png", "time": "3000","size_mean": "4", "size_sd": "0.5"
+    "species" : "ダンゴウオ", "size_out1": "3", "size_out2": "2", "size_in": "75", "pic": "gazou/favi.png", "time": "3000","size_mean": "4", "size_sd": "0.5"
   },
   {
-    "species" : "ミズダコ", "size_out1": "25", "size_out2": "6", "size_in": "95", "pic": "gazou/mizu.png", "time": "4000", "size_mean": "15","size_sd": "5"
+    "species" : "ミズダコ", "size_out1": "18", "size_out2": "4", "size_in": "95", "pic": "gazou/mizu.png", "time": "2500", "size_mean": "15","size_sd": "5"
   },
   {
-    "species" : "スズメダイ", "size_out1": "3", "size_out2": "3", "size_in": "90", "pic": "gazou/suzu.png", "time": "2000","size_mean": "6.5", "size_sd": "1.75"
+    "species" : "スズメダイ", "size_out1": "3", "size_out2": "2", "size_in": "90", "pic": "gazou/suzu.png", "time": "2000","size_mean": "6.5", "size_sd": "1.75"
   }
 ]
+// headerスライドショー
+jQuery(function($) {
+  $('.div_header_1').bgSwitcher({
+   images: ['gazou/index_1.jpg','gazou/index_2.JPG','gazou/index_3.JPG'], 
+   interval: 5000,
+   loop: true,
+   shuffle: true,
+   effect: "fade",
+   duration: 500,
+   easing: "swing"
+  });
+});
+
+// ゲームの遊び方スライドショー
+jQuery(function($) {
+  $('.tutorial_gazou_2').bgSwitcher({
+   images: ['gazou/index_tutorial_0.png','gazou/index_tutorial_1.png','gazou/index_tutorial_2.png','gazou/index_tutorial_3.png','gazou/index_tutorial_4.png','gazou/index_tutorial_5.png'], 
+   interval: 2500,
+   loop: true,
+   shuffle: false,
+   effect: "fade",
+   duration: 500,
+   easing: "swing"
+  });
+});
+
+
 // グラフ初期値
-var hist_range = 36
+var hist_range = 26
 var x0 = 5
 const chartElem1 = document.getElementById('chart1')
 const chartElem2 = document.getElementById('chart2')
 chartElems = [chartElem1, chartElem2]
 // 空のグラフ作成
 var hist_data = new Array(hist_range).fill(0);
-make_hist_chart(hist_data, hist_range, x0, chartElem1);
+make_hist_chart(hist_data, hist_data, hist_range, x0, chartElem1);
 make_bar(catch_records1, ap_fish2, chartElem2);
 
 // クリック等のイベントで実行
@@ -113,16 +146,23 @@ mediaElem2.addEventListener('timeupdate',function(){
 // 関数
 // ゲームのメイン関数（魚の出現，記録）
 function game_main(mediaElem, catch_records, hist_range, x0, counts, ap_fish, ap_record, game_id){
-  var playTimeBox = document.getElementById(`sec${game_id}`);
-  var playTime = mediaElem.duration - mediaElem.currentTime;
-  var playTimeDisplay = Math.round(playTime);
-  playTimeBox.innerHTML = playTimeDisplay;
-  var appearance = Math.floor( Math.random() * 101 );
-  if(appearance <= 40){
-    appearfish(catch_records, counts, ap_fish, ap_record, game_id);
+  var currentTime = mediaElem.currentTime
+  // ゲーム開始後（3秒後から）実行される関数
+  if(currentTime >= 2.8){
+    var status = document.getElementById(`status${game_id}`);
+    status.style.display = "block";
+    var playTimeBox = document.getElementById(`sec${game_id}`);
+    var playTime = mediaElem.duration - currentTime;
+    var playTimeDisplay = Math.round(playTime);
+    playTimeBox.innerHTML = playTimeDisplay;
+    var appearance = Math.floor( Math.random() * 101 );
+    if(appearance <= 30){
+      appearfish(catch_records, counts, ap_fish, ap_record, game_id, currentTime);
+    }
   }
+  
   // 終了したときの関数
-  if(mediaElem.duration == mediaElem.currentTime){
+  if(mediaElem.duration == currentTime){
     mediaElem.pause()
     var game_finish = document.getElementById(`game_finish${game_id}`);
     game_finish.style.display = "block";
@@ -130,7 +170,7 @@ function game_main(mediaElem, catch_records, hist_range, x0, counts, ap_fish, ap
     size = [];
     game_list_id = game_id - 1;
     if (game_id == 1) {
-      make_hist(catch_records, hist_range, x0, chartElems[game_list_id]);      
+      make_hist(catch_records, hist_range, x0, chartElems[game_list_id]);
     }else if(game_id == 2){
       // make_hist(catch_records, hist_range, x0, chartElems[game_list_id]);
       make_bar(catch_records, ap_fish, chartElems[game_list_id]);
@@ -139,7 +179,11 @@ function game_main(mediaElem, catch_records, hist_range, x0, counts, ap_fish, ap
       size.push(catch_records[i].size);
       species.push(catch_records[i].species);
       var li = document.createElement('li');
-      li.textContent=`${i+1}.${catch_records[i].species} (${catch_records[i].size}cm)`;
+      if(catch_records[i].species=="ミズダコ"){
+        li.textContent=`${i+1}.${catch_records[i].species} (${catch_records[i].size}kg)`;
+      }else{
+        li.textContent=`${i+1}.${catch_records[i].species} (${catch_records[i].size}cm)`;
+      }
       document.getElementById(`li_records${game_id}`).appendChild(li);
       var total = catch_records.length;
       var gyosyu = species.filter((elem, index, self) => self.indexOf(elem) === index);
@@ -153,36 +197,50 @@ function game_main(mediaElem, catch_records, hist_range, x0, counts, ap_fish, ap
   };
 }
 
+
 // ヒストグラム作成関数(釣果，x軸幅，x軸最小値，グラフ入力要素)
 function make_hist(catch_records, hist_range, x0, histElem){
   // 参照渡し
-  var gayas = catch_records.filter(v => v.species === "エゾメバル");
+  var gayas_osu = catch_records.filter(v => v.sex === 0);
+  var gayas_mesu = catch_records.filter(v => v.sex === 1);
   //値渡し
-  gayas = JSON.stringify(gayas);
-  gayas = JSON.parse(gayas);
-  // 切り捨て
-  gayas_size = gayas.map(v => Math.floor(v.size));
+  gayas_osu = JSON.stringify(gayas_osu);
+  gayas_osu = JSON.parse(gayas_osu);
+  gayas_mesu = JSON.stringify(gayas_mesu);
+  gayas_mesu = JSON.parse(gayas_mesu);
+  
+  // 体長，小数点切り捨て
+  gayas_size_osu = gayas_osu.map(v => v = Math.floor(v.size) - x0);
+  gayas_size_mesu = gayas_mesu.map(v => v = Math.floor(v.size) - x0);
   // ヒストグラムデータ作成
-  var hist_data_1 = new Array(hist_range).fill(0);
-  gayas_size.map(v => hist_data_1[v]++);
-  make_hist_chart(hist_data_1, hist_range, x0, histElem);
+  var hist_data_osu = new Array(hist_range + x0).fill(0);
+  var hist_data_mesu = new Array(hist_range + x0).fill(0);
+  gayas_size_osu.map(v => hist_data_osu[v]++);
+  gayas_size_mesu.map(v => hist_data_mesu[v]++);
+  make_hist_chart(hist_data_osu, hist_data_mesu, hist_range, x0, histElem);
 }
 
 // ヒストグラム作成関数(階級データ，x軸幅，x軸最小値，グラフ入力要素)
-function make_hist_chart(hist_data_1, hist_range, x0, histElem){
-  var hist_data_2 = {
+function make_hist_chart(hist_data_osu, hist_data_mesu, hist_range, x0, histElem){
+  var hist_data = {
     labels: [...Array(hist_range).keys()].map(v => v += x0),
     datasets: [{
-    label:"エゾメバル",
-    backgroundColor: "#5d94e7",
+    label:"オス",
+    backgroundColor: "rgb(0, 90, 200)",
     borderWidth: `${3}vw`,
-    data: hist_data_1
-    }]
+    data: hist_data_osu
+    },
+    {
+      label:"メス",
+      backgroundColor: "rgb(140, 200, 240)",
+      borderWidth: `${3}vw`,
+      data: hist_data_mesu
+      }]
   }
   // ヒストグラム作成
   new Chart(histElem, {
     type: 'bar',
-    data: hist_data_2,
+    data: hist_data,
     options: {
       title: {
         display: true,
@@ -191,7 +249,7 @@ function make_hist_chart(hist_data_1, hist_range, x0, histElem){
       scales: {
         yAxes: [{
           ticks: {
-            suggestedMax: 20,
+            suggestedMax: 15,
             suggestedMin: 0,
             stepSize: 1
           }
@@ -202,17 +260,27 @@ function make_hist_chart(hist_data_1, hist_range, x0, histElem){
 }
 
 
+// 積み立て棒グラフ作成関数(釣果, 出現魚リスト, グラフ表示場所)
 function make_bar(catch_records, ap_fish, barElem){
-  var catch_species = catch_records.map(v => v.species);
   var ap_species = ap_fish.map(v => v.species);
+  var area1_records = catch_records.filter(v => v.time <= 23);
+  var area2_records = catch_records.filter(v => v.time > 23 & v.time <=43);
+  var area3_records = catch_records.filter(v => v.time > 43);
+  var areas_records = [area1_records, area2_records, area3_records];
+  
   bar_data = [];
   color_bar = ["rgb(0, 20, 90)", "rgb(0, 40, 130)", "rgb(0, 60, 170)", "rgb(0, 90, 200)", "rgb(40, 120, 200)", "rgb(60, 140, 220)", "rgb(100, 170, 240)", "rgb(140, 200, 240)", "rgb(160, 230, 255)", "rgb(90, 220, 255)", "rgb(50, 200, 255)","rgb(10, 220, 255)"]
   for (var id=0; id < ap_species.length; id++) {
-    var species_num = [];
-    // 繰り返す
+
+    // エリアごとに釣果を格納
     var species_nums = [];
-    species_num = catch_species.filter(v => v === ap_species[id]).length;
-    species_nums.push(species_num);
+    for(var i=0; i<areas_records.length; i++){
+      var species_num = 0;
+      var area_records = areas_records[i];
+      // 魚種ごとの漁獲数をspecies_numに格納
+      species_num = area_records.filter(v => v.species === ap_species[id]).length;
+      species_nums.push(species_num);
+    }
 
     var species_data = {};
     species_data.label = ap_species[id];
@@ -225,18 +293,18 @@ function make_bar(catch_records, ap_fish, barElem){
   make_bar_chart(bar_data, barElem);
 }
 
-// 積み立て棒グラフ作成関数(データ，)
+// 積み立て棒グラフ作成関数(データ, グラフ表示場所)
 function make_bar_chart(bar_data, barElem){
   new Chart(barElem, {
     type: 'bar',
     data: {
-      labels: ["砂地"],
+      labels: ["エリア1","エリア2", "エリア3"],
       datasets: bar_data,
     },
     options: {
       title: {
         display: true,
-        text: '地点別釣果'
+        text: '地点ごとの魚種別釣果'
       },
       scales: {
         xAxes: [{
@@ -259,9 +327,31 @@ function make_bar_chart(bar_data, barElem){
 
 
 // 魚出現関数
-function appearfish(catch_records, counts, ap_fish, ap_record, game_id){
+function appearfish(catch_records, counts, ap_fish, ap_record, game_id, currentTime){
   // 魚種選択
-  var id = Math.floor( Math.random() * ap_fish.length );
+  if(game_id==2){
+    if(currentTime<=22.5){
+      ap_fish_area = [0,1,4];
+      var id = ap_fish_area[Math.floor( Math.random() * ap_fish_area.length)];
+    }else if(currentTime>22.5 & currentTime<=42.5){
+      if(currentTime<=23.5){
+        document.getElementById(`div_apfish${game_id}`).textContent = null;
+        return;
+      }
+      ap_fish_area = [0,2,4,6,7,8,9];
+      var id = ap_fish_area[Math.floor( Math.random() * ap_fish_area.length)];
+    }else if(currentTime>42.5){
+      if(currentTime<=43.5){
+        document.getElementById(`div_apfish${game_id}`).textContent = null;
+        return;
+      }
+      ap_fish_area = [0,3,5,7]
+      var id = ap_fish_area[Math.floor( Math.random() * ap_fish_area.length)];
+    }
+  }else{
+    var id = Math.floor( Math.random() * ap_fish.length );
+  }
+
   // div取得（基準位置）
   var div_apfish = document.getElementById(`div_apfish${game_id}`);
   // 個体作成div
@@ -280,7 +370,7 @@ function appearfish(catch_records, counts, ap_fish, ap_record, game_id){
   fishclass = "circle div_apfish_out";
   div_apfish_out.className = fishclass;
   div_apfish_out.onclick=function(e){
-    catch_fish(e.target.parentNode, catch_records, ap_fish, ap_record, game_id);
+    catch_fish(e.target.parentNode, catch_records, ap_fish, ap_record, game_id, currentTime);
   };
   div_apfish_ind.appendChild(div_apfish_out);
   // 内枠サイズdiv
@@ -326,10 +416,11 @@ function appearfish(catch_records, counts, ap_fish, ap_record, game_id){
 }
 
 // 漁獲関数
-function catch_fish(div_apfish_ind, catch_records, ap_fish, ap_record, game_id){
+function catch_fish(div_apfish_ind, catch_records, ap_fish, ap_record, game_id, currentTime){
+  // 釣れたことを表示するアクション追加
   var id = div_apfish_ind.id;
   var fish = ap_record.find((v) => v.id === id);
-  add_records1(catch_records, fish.species, ap_fish, game_id);
+  add_records_1(catch_records, fish.species, ap_fish, game_id, currentTime);
   div_apfish_ind.remove();
 }
 
@@ -339,37 +430,44 @@ function avoid_fish(div_apfish_ind){
   event.stopPropagation();
 }
 
-function add_records1(catch_records, species, ap_fish, game_id) {
+function add_records_1(catch_records, species, ap_fish, game_id, currentTime) {
   // 魚類
   var species = species;
   // サイズ
   var fish = ap_fish.find((v) => v.species === species);
   var mean = parseInt(fish.size_mean);
   var sd = parseInt(fish.size_sd);
+  var sex = 0
   // 配列に追加
-  if(species == "エゾメバル" & game_id == 1){
-    for (let i = 0; i < 10; i++) {
+  if(game_id == 1){
+    var counts = species.replace(/[^0-9]/g, '');
+    for (let i = 0; i < counts; i++) {
       var rand = Math.random() * 101;
-      if(rand <= 30){
-        mean += 10
-        add_records2(catch_records, species, mean, sd)
-        mean -= 10
-      }else{add_records2(catch_records, species, mean, sd)
+      if(rand <= 50){
+        var sex = 1
+        mean += 3
+        add_records_2(catch_records, "エゾメバル", mean, sd, game_id, currentTime, sex)
+        mean -= 3
+      }else{add_records_2(catch_records, "エゾメバル", mean, sd, game_id, currentTime, sex)
       };  
     };
   }else{
-    add_records2(catch_records, species, mean, sd)
+    add_records_2(catch_records, species, mean, sd, game_id, currentTime, sex)
   };
-  // 漁獲日数更新
+  // 漁獲数更新
   var num = document.getElementById(`num${game_id}`);
   num.innerHTML = catch_records.length;
 }
 
-function add_records2(catch_records, species, mean, sd){
+function add_records_2(catch_records, species, mean, sd, game_id, currentTime, sex){
   // サイズ決定
   var size = mean + rnorm() * sd
   // 配列に格納
-  catch_records.push({species: species, size: Math.floor(size *100)/100});
+  if(game_id == 1){
+    catch_records.push({species: species, size: Math.floor(size *100)/100, sex: sex});
+  }else{
+    catch_records.push({species: species, size: Math.floor(size*100)/100, time: Math.floor(currentTime*100)/100});
+  }
 }
 
 // 正規乱数作成
